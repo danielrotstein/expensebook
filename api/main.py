f02fe75eebce9f99c1aca8ed45395072508baaa7
Olivia Sun <apinet2003@gmail.com>
2022-10-06 15:41:55 -0400

    code modified

--- api/main.py
+++ api/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from routers import expenses, accounts, budgets, categories
 
 app = FastAPI()
-app.include_router(expenses.router)
 
 app.include_router(accounts.router)
 app.include_router(categories.router)
