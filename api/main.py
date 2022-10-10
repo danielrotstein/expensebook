ae5496cf31d7557c98c014b9ef802a16ed3a2a4c
George Franco <gh.franco.98@gmail.com>
2022-10-10 19:31:04 -0400

    removed auth files

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routers import expenses, accounts, budgets, categories, auth
+from routers import expenses, accounts, budgets, categories, recommendations
 from authenticator import authenticator
 
 app = FastAPI()
@@ -9,12 +9,5 @@ app.include_router(accounts.router)
 app.include_router(categories.router)
 app.include_router(budgets.router)
 app.include_router(expenses.router)
-<<<<<<< HEAD
-
-app.include_router(authenticator.router)
-app.include_router(auth.router)
-
-=======
 app.include_router(recommendations.router)
 app.include_router(authenticator.router)
->>>>>>> 16f5eaa302a3e62c24eb1a7facb9abac00d53487
