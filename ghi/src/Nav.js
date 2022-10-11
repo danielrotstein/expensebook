0455976912cbf07706f234e9055b74ffa52a3d00
Joey <joey.salvo.dev@gmail.com>
2022-10-11 15:15:46 -0700

    chenged navbar color theme

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 function Nav() {
     return (
         <div>
-            <nav className='navbar navbar-expand-lg navbar-dark bg-black'>
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
                 <div className="container-fluid">
                     <NavLink className="navbar-brand" to="/">ExpenseBook</NavLink>
-                    <NavLink className="navbar-brand" to="/budgets">Budgets</NavLink>
+                    <NavLink className="navbar-brand btn" to="/budgets">Budgets</NavLink>
                 </div>
             </nav>
         </div>
