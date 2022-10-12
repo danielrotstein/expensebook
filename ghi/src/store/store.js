14fc5b1b279b5f11a6f53af639029cf49d6d5efe
George Franco <gh.franco.98@gmail.com>
2022-10-12 18:07:35 -0400

    changes to store

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -2,19 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { budgetsApi } from './budgetsApi';
 import { accountsApi } from './accountsApi';
-import { tokensApi } from './tokensApi';
 
 export const store = configureStore({
   reducer: {
     [budgetsApi.reducerPath]: budgetsApi.reducer,
     [accountsApi.reducerPath]: accountsApi.reducer,
-    [tokensApi.reducerPath]: tokensApi.reducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware()
       .concat(budgetsApi.middleware)
       .concat(accountsApi.middleware)
-      .concat(tokensApi.middleware)
 });
 
 setupListeners(store.dispatch);
