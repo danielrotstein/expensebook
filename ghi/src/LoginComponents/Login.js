e12f8030559b7f5f941c918cf1bd95e1a5f004d4
George Franco <gh.franco.98@gmail.com>
2022-10-13 16:13:04 -0400

    signup form now working

--- ghi/src/LoginComponents/Login.js
+++ ghi/src/LoginComponents/Login.js
@@ -9,8 +9,6 @@ function LogIn() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    // const [first_name, setFirstName] = useState('');
-    // const [last_name, setLastName] = useState('');
     const [error, setError] = useState('');
     const [createToken, result] = useCreateTokenMutation();
     
@@ -44,20 +42,6 @@ function LogIn() {
                             required placeholder="Password" type="password" name="password" id="password" 
                             className="form-control"/>
                         </div>
-
-                        {/* <div className="mb-3">
-                            <label htmlFor="first_name">First Name</label>
-                            <BulmaInput onChange={setFirstName} value={first_name.first_name} required placeholder="first_name" type="text" name="first_name" id="first_name" className="form-control"/>
-                        </div>
-
-                        <div className="mb-3">
-                            <label htmlFor="last_name">Last Name</label>
-                            <BulmaInput onChange={setLastName} value={last_name.last_name} required placeholder="last_name" type="text" name="last_name" id="last_name" className="form-control"/>
-                        </div> */}
-
-
-
-                        
                         <div className="field">
                             <button className="btn btn-primary">Save</button>
                         </div>
