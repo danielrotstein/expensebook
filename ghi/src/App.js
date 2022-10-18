44cad1679530e4c4575e6862a3b44aa40d9d29b6
daniel-san17 <daniel.rotstein17@gmail.com>
2022-10-18 18:35:59 -0400

    uncomment footer in App.js again - not staying fix at bottom

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -31,7 +31,7 @@ function App() {
         </Route>
         <Route path='$converter' element={< CurrencyConverter/>} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
