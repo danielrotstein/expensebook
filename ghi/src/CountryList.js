f896bcc43f2c6898fd6c76cc0c39edf6d2b3d2c5
daniel-san17 <daniel.rotstein17@gmail.com>
2022-10-19 19:48:43 -0400

    modified CountryList.js

--- ghi/src/CountryList.js
+++ ghi/src/CountryList.js
@@ -16,7 +16,7 @@ const countries = [
         "currency_code": "USD"
     },
     {
-        "name": "Andorra, Albania, Belgium, Cyprus, Estonia, Finland, French Guiana",
+        "name": "Andorra, Albania, Belgium, Cyprus, Estonia, Finland, French Guiana, French Southern territories",
         "currency_code": "EUR"
     },
     {
