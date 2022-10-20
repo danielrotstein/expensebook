eb5c27d823095e725e99a60ecbe6f5aed3c0713b
Olivia Sun <apinet2003@gmail.com>
2022-10-20 16:07:27 -0400

    test

--- ghi/src/BudgetComponents/BudgetDashboard.js
+++ ghi/src/BudgetComponents/BudgetDashboard.js
@@ -50,7 +50,7 @@ function BudgetDashboard() {
                 </>
                 : 
                 <>
-                    navigate("/about")
+                    navigate("/budgets/add-budget")
                 </>
                 }
             </div>
