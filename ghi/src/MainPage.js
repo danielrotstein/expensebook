a407dfdfe2ede6cb7d39085a3c4bdf7c9b14b325
daniel-san17 <daniel.rotstein17@gmail.com>
2022-10-20 19:42:57 -0400

    changed currency converter to converter in MainPage.js

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 function MainPage() {
     return (
         <div className="container-fluid">
-            <NavLink className="nav-link" to="/$converter">Currency Converter</NavLink>
+            <NavLink className="nav-link" to="/$converter">Converter</NavLink>
             <h1>Main Page Content</h1>
         </div>
     )
