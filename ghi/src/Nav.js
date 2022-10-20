4d4cfc722dcb9554a4aa589c3d637144b856eb14
daniel-san17 <daniel.rotstein17@gmail.com>
2022-10-20 15:45:49 -0400

    cleaned code in Nav.js

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -41,28 +41,28 @@ function Nav() {
                             ? <></>
                             : token
                               ? <>                            
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/budgets">Budgets</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <Logout className="nav-link" />
-                                </li>
-                                <li className="nav-item">
-                                  <NavLink className="nav-link" to="/$converter">Currency Converter</NavLink>
-                                </li>
-                              </>
+                                  <li className="nav-item">
+                                      <NavLink className="nav-link" to="/budgets">Budgets</NavLink>
+                                  </li>
+                                  <li className="nav-item">
+                                      <Logout className="nav-link" />
+                                  </li>
+                                  <li className="nav-item">
+                                    <NavLink className="nav-link" to="/$converter">Currency Converter</NavLink>
+                                  </li>
+                                </>
                               : 
-                              <>
-                                <li className="nav-item">
-                                  <NavLink className="nav-link" to="/$converter">Currency Converter</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/login">Login</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
-                                </li>                              
-                              </>
+                                <>
+                                  <li className="nav-item">
+                                    <NavLink className="nav-link" to="/$converter">Currency Converter</NavLink>
+                                  </li>
+                                  <li className="nav-item">
+                                      <NavLink className="nav-link" to="/login">Login</NavLink>
+                                  </li>
+                                  <li className="nav-item">
+                                      <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
+                                  </li>                              
+                                </>
                             }
                         </ul>
                     </div>
