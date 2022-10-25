eb8d5b2a50fd333269904afd08d2669967229949
Joey <joey.salvo.dev@gmail.com>
2022-10-25 10:00:49 -0700

    navbar changes - removed li tags - set nav to default

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -4,14 +4,14 @@ import { useEffect } from 'react';
 
 function Logout() {
     const [logOut, { data }] = useLogOutMutation();
-  
+
 
     useEffect(() => {
       if (data) {
         localStorage.clear();
       }
     }, [data]);
-  
+
 
     return (
         <div className="buttons">
@@ -21,50 +21,32 @@ function Logout() {
         </div>
     );
   }
-  
+
 
 function Nav() {
     const { data: token, isLoading: tokenLoading } = useGetTokenQuery();
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg">
+        <div className='custom-nav'>
+            <nav className="navbar navbar-expand-sm">
                 <div className="container-fluid">
                     <NavLink className="navbar-brand" to="/">ExpenseBook</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="navbar-default">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                           {tokenLoading
                             ? <></>
                             : token
-                              ? <>                            
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/budgets">Budgets</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                  <NavLink className="nav-link" to="/$converter">Converter</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <Logout className="nav-link" />
-                                </li>
-                              </>
-                              : 
-                              <>
-                                <li className="nav-item">
-                                  <NavLink className="nav-link" to="/$converter">Converter</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/login">Login</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" id="signup" to="/signup">Sign Up</NavLink>
-                                </li>     
-                                <script type="text/javascript">
-                                    document.body.innerHTML = '';
-                                </script>                         
-                              </>
-                            }
+                            ? <>
+                                <NavLink className="nav-link" to="/budgets">Budgets</NavLink>
+                                <NavLink className="nav-link" to="/$converter">Converter</NavLink>
+                                <Logout className="nav-link" />
+                            </>
+                            :
+                            <>
+                                <NavLink className="nav-link" to="/$converter">Converter</NavLink>
+                                <NavLink className="nav-link" to="/login">Login</NavLink>
+                                <NavLink className="nav-link" id="signup" to="/signup">Sign Up</NavLink>
+                            </>
+                          }
                         </ul>
                     </div>
                 </div>
